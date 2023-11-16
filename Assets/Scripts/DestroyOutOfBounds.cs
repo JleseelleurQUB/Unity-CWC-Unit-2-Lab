7a7dad4bfd5d41b8c848aeed19ca7459e099d641
JleseelleurQUB <josephleseelleur@gmail.com>
2023-11-16 17:00:29 +0000

    6.1.2
    - CWC Unit 6.1
    
    - updated scripts to package versions, pursuant to replacing instantiation of prefabs with object pooling method

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,37 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Destroys objects that leave the player's view
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
-        } 
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
-        }           
-
-        // Reduces score when animals pass the player
-            if (transform.position.z < lowerBound && DetectCollisions.lives > 0)
-        {
-            DetectCollisions.lives--;
-            Debug.Log("Lives = " + DetectCollisions.lives);
         }
-           
+
     }
 }
--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -1,56 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class DetectCollisions : MonoBehaviour
 {
-    // static allows variable to be accessed by other scripts
-    public static int score = 0;
-    public static int lives = 3;
-    // slider UI element to track animal health
-    public Slider slider;
-    public int health;
-
-    // Sets slider max and current value for each type of animal upon spawning
-    private void Start()
+    void OnTriggerEnter(Collider other)
     {
-        slider.maxValue = health;
-        slider.value = health;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        // Destroys projectiles and animals upon collision, reduces/increases player lives/score based on what is colliding. Name may be used as identifier, or general tags assigned via inspector
-        
-        if (other.gameObject.name == "Player" && lives > 0)
-        {
-            lives--;
-            Destroy(gameObject);
-            Debug.Log("Lives = " + lives);
-        }
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
 
-        else if (other.gameObject.tag != "Player")
-        {
-            slider.value--;
-            Destroy(other.gameObject);
-            // If health slider is at 0, animal is destroyed by collision and score increases
-            if (slider.value < 1)
-            {
-                score++;
-                Destroy(gameObject);
-                Debug.Log("Score = " + score);
-            }
-        }
-
-      
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 
-    private void Update()
-    {
-        if (lives == 0)
-        {
-            Debug.Log("Game Over");
-        }
-    }
 }
